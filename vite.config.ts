5dd6902483326b21296af2d94f885ef7eac46543
zhaxnb <zhaxnb@zhaxnb.com>
2023-09-13 18:10:26 +0800

    配置文件新增基础配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,73 @@
-import { fileURLToPath, URL } from 'node:url'
+import { UserConfig, ConfigEnv, loadEnv, defineConfig } from 'vite';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import createPlugins from './plugins';
+
+import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode, command }: ConfigEnv): UserConfig => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    // 默认情况下vite都会把应用部署到“/”根路径上，可以使用此配置选项设置子路径。
+    base: env.VITE_APP_CONTEXT_PATH,
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './'),
+        '@': path.resolve(__dirname, './src'),
+        // 'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js'
+      },
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+    },
+    plugins: createPlugins(env, command === 'build'),
+    server: {
+      host: '0.0.0.0',
+      port: Number(env.VITE_APP_PORT),
+      open: true,
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          target: 'http://localhost:8080',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(new RegExp('^' + env.VITE_APP_BASE_API), '')
+        }
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          javascriptEnabled: true
+        }
+      },
+      postcss: {
+        plugins: [
+          {
+            postcssPlugin: 'internal:charset-removal',
+            AtRule: {
+              charset: (atRule) => {
+                if (atRule.name === 'charset') {
+                  atRule.remove();
+                }
+              }
+            }
+          }
+        ]
+      }
+    },
+    // 预编译
+    optimizeDeps: {
+      include: [
+        'vue',
+        'vue-router',
+        'pinia',
+        'axios',
+        // '@vueuse/core',
+        // 'path-to-regexp',
+        // 'echarts',
+        // '@wangeditor/editor',
+        // '@wangeditor/editor-for-vue',
+        // 'vue-i18n',
+        // '@vueup/vue-quill',
+        // '@iconify/iconify',
+      ]
     }
   }
 })
